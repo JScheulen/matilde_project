c115e3aaadc9c2c7d1d29cde4ad959ecc682d4d3
JScheulen <152833532+JScheulen@users.noreply.github.com>
2024-01-18 23:34:11 -0300

    se agrego base de datos y se hicieron migraciones

---
+++ app/erp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Productos(models.Model):
+    codigo = models.CharField(max_length=255, unique=True)
+    nombre = models.CharField(max_length=255)
+    cantidad = models.IntegerField(default=1)
+    precio = models.DecimalField(default=0.00, decimal_places=2, max_digits=9)
+    actividad = models.CharField(max_length=50)
+    foto = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        db_table = 'nuevo_producto'
