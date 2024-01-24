17f9ac622c1d9043f7a279e7e021fb8aef8a83fe
JScheulen <152833532+JScheulen@users.noreply.github.com>
2024-01-23 23:19:01 -0300

    se modifico la categoria

--- app/erp/views.py
+++ app/erp/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from erp.models import Productos
+
 
 # Create your vistas here.
 def Inicio(request):
-    return render(request, 'inicio.html')
+    data = {
+        'categories': Productos.objects.all()
+    }
+    return render(request, 'inicio.html', data)
+
+
